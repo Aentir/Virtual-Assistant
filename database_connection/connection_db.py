72bf7e45551bd65ee424b2c3ef1f6ccdf32e9f6e
JL.Lloret <75336255+Aentir@users.noreply.github.com>
2022-06-21 13:19:06 +0200

    Updated connection.py file

--- database_connection/connection_db.py
+++ database_connection/connection_db.py
@@ -3,7 +3,7 @@ import mysql.connector
 def connection():
     connDb = mysql.connector.connect(
         user='root',
-        password='freebord2912',
+        password='',
         host='127.0.0.1',
         database='virtual_assistant',
         port='3306')
